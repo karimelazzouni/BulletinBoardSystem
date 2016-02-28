9db11b7b0b89018b6e6117baa070afc51cdfcda3
karim <k.elazzouni@live.com>
2016-02-28 02:28:47 +0200

    clean repo

--- src/BulletinBoardReader.java
+++
@@ -1,7 +0,0 @@
-import java.io.FileNotFoundException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface BulletinBoardReader extends Remote {
-	public String read(String rid) throws RemoteException,FileNotFoundException;
-}
--- src/BulletinBoardWriter.java
+++
@@ -1,7 +0,0 @@
-import java.io.FileNotFoundException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface BulletinBoardWriter extends Remote {
-	public String write(String wid, String news) throws RemoteException,FileNotFoundException;
-}
